ff8ef60be3a43ed3a0a86df98a4bdbb08a91966b
Kyle Hernandez <kylesdev91@yahoo.com>
2022-11-23 12:33:17 -0600

    DONE WITH FIRST LOCATION APP

--- src/pages/UserLocation.vue
+++ src/pages/UserLocation.vue
@@ -1,25 +1,38 @@
 <template>
-  <section class="ui two column centered grid">
-    <div class="column">
-      <form class="ui segment large form">
-        <div class="ui message red" v-show="error">{{ error }}</div>
-        <div class="ui segment">
-          <div class="field">
-            <div class="ui right icon input large" :class="{loading:spinner}">
-              <input
-                type="text"
-                placeholder="Enter your address"
-                v-model="address"
-                id="autocomplete"
-              />
-              <i class="dot circle link icon" @click="locatorButtonPressed"></i>
+  <div>
+    <section
+      class="ui two column centered grid"
+      style="position:relative;z-index:1;"
+    >
+      <div class="column">
+        <form class="ui segment large form">
+          <div class="ui message red" v-show="error">{{ error }}</div>
+          <div class="ui segment">
+            <div class="field">
+              <div
+                class="ui right icon input large"
+                :class="{ loading: spinner }"
+              >
+                <input
+                  type="text"
+                  placeholder="Enter your address"
+                  v-model="address"
+                  id="autocomplete"
+                />
+                <i
+                  class="dot circle link icon"
+                  @click="locatorButtonPressed"
+                ></i>
+              </div>
             </div>
+            <button class="ui button">Go</button>
           </div>
-          <button class="ui button">Go</button>
-        </div>
-      </form>
-    </div>
-  </section>
+        </form>
+      </div>
+    </section>
+
+    <section id="map"></section>
+  </div>
 </template>
 
 <script>
@@ -30,19 +43,23 @@ export default {
     return {
       address: "",
       error: "",
-      spinner: false,
+      spinner: false
     };
   },
 
   mounted() {
-    new google.maps.places.Autocomplete(
-       document.getElementById("autocomplete"),
-       {
-         bounds: new google.maps.LatLngBounds(
-           new google.maps.LatLng(33.10317, -96.67055)
-         )
-       }
-    )
+    let autocomplete = new google.maps.places.Autocomplete(
+      document.getElementById("autocomplete"),
+      {
+        bounds: new google.maps.LatLngBounds(
+          new google.maps.LatLng(33.10317, -96.67055)
+        )
+      });
+      autocomplete.addListener("place_changed", () => {
+        let place = autocomplete.getPlace();
+        console.log(place);
+        this.showUserLocationOnTheMap(place.geometry.location.lat(), place.geometry.location.lng())
+      })
   },
 
   methods: {
@@ -55,9 +72,14 @@ export default {
               position.coords.latitude,
               position.coords.longitude
             );
+            this.showUserLocationOnTheMap(
+              position.coords.latitude,
+              position.coords.longitude
+            );
           },
           error => {
-            this.error = "Locator is unable to find your address. Please type your address manually.";
+            this.error =
+              "Locator is unable to find your address. Please type your address manually.";
             this.spinner = false;
             // console.log(error.message);
           }
@@ -91,6 +113,19 @@ export default {
           this.error = error.message;
           console.log(error.message);
         });
+    },
+    showUserLocationOnTheMap(latitude, longitude) {
+      // Create A Map Object
+      let map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15,
+        center: new google.maps.LatLng(latitude, longitude),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      });
+      // Add Marker
+      new google.maps.Marker({
+        position: new google.maps.LatLng(latitude, longitude),
+        map: map
+      })
     }
   }
 };
@@ -117,8 +152,16 @@ export default {
   background-color: #ececec;
 }
 
-
 .pac-item-query {
   font-size: 16px;
+}
+
+#map {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: red;
 }
 </style>
