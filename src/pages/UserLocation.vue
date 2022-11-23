9f963bd6c70901acc9ab9b805a4ade2b288ce19e
Kyle Hernandez <kylesdev91@yahoo.com>
2022-11-23 09:14:42 -0600

    Section 2, Lecture 6

--- src/pages/UserLocation.vue
+++ src/pages/UserLocation.vue
@@ -1,21 +1,43 @@
 <template>
   <section class="ui two column centered grid">
     <div class="column">
-        <form class="ui segment large form">
-            <div class="ui message red"></div>
-            <div class="ui segment">
-                <div class="field">
-                    <div class="ui right icon input large">
-                        <input type="text" placeholder="Enter your address" />
-                        <i class="circle icon"></i>
-                    </div>
-                </div>
-                <button class="ui button">Go</button>
+      <form class="ui segment large form">
+        <div class="ui message red"></div>
+        <div class="ui segment">
+          <div class="field">
+            <div class="ui right icon input large">
+              <input type="text" placeholder="Enter your address" />
+              <i class="dot circle link icon" @click="locatorButtonPressed"></i>
             </div>
-        </form>
+          </div>
+          <button class="ui button">Go</button>
+        </div>
+      </form>
     </div>
   </section>
 </template>
+
+<script>
+export default {
+  methods: {
+    locatorButtonPressed() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          position => {
+            console.log(position.coords.latitude);
+            console.log(position.coords.longitude);
+          },
+          error => {
+            console.log(error.message);
+          }
+        );
+      } else {
+        console.log("Your browser does not support geolocation API");
+      }
+    }
+  }
+};
+</script>
 
 <style>
 .ui.button,
